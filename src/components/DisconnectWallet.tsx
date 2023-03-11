6cf47ee2fe3c39473de25eb8991c2035c327c4ea
Benas Volkovas <benasvol@gmail.com>
2023-03-11 17:06:11 +0200

    Initial clean up

--- src/components/DisconnectWallet.tsx
+++ src/components/DisconnectWallet.tsx
@@ -3,44 +3,41 @@ import { BeaconWallet } from "@taquito/beacon-wallet";
 import { TezosToolkit } from "@taquito/taquito";
 
 interface ButtonProps {
-  wallet: BeaconWallet | null;
-  setPublicToken: Dispatch<SetStateAction<string | null>>;
-  setUserAddress: Dispatch<SetStateAction<string>>;
-  setUserBalance: Dispatch<SetStateAction<number>>;
-  setWallet: Dispatch<SetStateAction<any>>;
-  setTezos: Dispatch<SetStateAction<TezosToolkit>>;
-  setBeaconConnection: Dispatch<SetStateAction<boolean>>;
+    wallet: BeaconWallet | null;
+    setPublicToken: Dispatch<SetStateAction<string | null>>;
+    setUserAddress: Dispatch<SetStateAction<string>>;
+    setWallet: Dispatch<SetStateAction<any>>;
+    setTezos: Dispatch<SetStateAction<TezosToolkit>>;
+    setbeaconConnectionActiveActive: Dispatch<SetStateAction<boolean>>;
 }
 
 const DisconnectButton = ({
-  wallet,
-  setPublicToken,
-  setUserAddress,
-  setUserBalance,
-  setWallet,
-  setTezos,
-  setBeaconConnection
+    wallet,
+    setPublicToken,
+    setUserAddress,
+    setWallet,
+    setTezos,
+    setbeaconConnectionActiveActive,
 }: ButtonProps): JSX.Element => {
-  const disconnectWallet = async (): Promise<void> => {
-    if (wallet) {
-      await wallet.clearActiveAccount();
-    }
-    setUserAddress("");
-    setUserBalance(0);
-    setWallet(null);
-    const tezosTK = new TezosToolkit("https://ghostnet.ecadinfra.com");
-    setTezos(tezosTK);
-    setBeaconConnection(false);
-    setPublicToken(null);
-  };
+    const disconnectWallet = async (): Promise<void> => {
+        if (wallet) {
+            await wallet.clearActiveAccount();
+        }
+        setUserAddress("");
+        setWallet(null);
+        const tezosTK = new TezosToolkit("https://ghostnet.ecadinfra.com");
+        setTezos(tezosTK);
+        setbeaconConnectionActiveActive(false);
+        setPublicToken(null);
+    };
 
-  return (
-    <div className="buttons">
-      <button className="button" onClick={disconnectWallet}>
-        <i className="fas fa-times"></i>&nbsp; Disconnect wallet
-      </button>
-    </div>
-  );
+    return (
+        <div className="buttons">
+            <button className="button" onClick={disconnectWallet}>
+                <i className="fas fa-times"></i>&nbsp; Disconnect wallet
+            </button>
+        </div>
+    );
 };
 
 export default DisconnectButton;
